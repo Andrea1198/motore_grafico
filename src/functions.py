acee1d955d43d9f342315ab2334f8ce7e77a4931
Andrea Pintus <andrew01011998@gmail.com>
2021-08-27 16:55:55 +0200

    Cambiamento della dinamica di movimento. Manca ancora da ottimizzare la visualizzazione dei muri

--- src/functions.py
+++ src/functions.py
@@ -10,19 +10,19 @@ class Line:
         y21 = self.y2-self.y1
         x21 = self.x2-self.x1
         if x21 != 0:
+            self.m  = y21/x21
             if y21*x21 > 0:
-                self.m  = atan(y21/x21)
+                self.a  = atan(self.m)
             else:
-                self.m  = -atan(y21/x21)
+                self.a  = -atan(self.m)
         else:
-            self.m = sys.max_int
+            self.m = 1e6
         self.q = self.y2 - self.m*self.x2
 
     def show(self, screen):
         import pygame
         white = (255, 255, 255)
         pygame.draw.line(screen, white, (self.x1, self.y1), (self.x2, self.y2))
-
 
 # class of Boxes in case you want to use boxes instead of lines
 class Box:
@@ -79,6 +79,10 @@ def check_dist(x1, x2, y1, y2):
     dist = sqrt(dist)
     return dist
 
+# class Player:
+#     def __init__(self):
+
+
 def start():
     import pygame
     from numpy import pi, cos, sin, sqrt
@@ -87,8 +91,8 @@ def start():
     n = 100
     delta = pi/n*2
     pygame.init()
-    WIDTH  = 800
-    HEIGHT = 800
+    WIDTH  = 1920
+    HEIGHT = 1080
     r      = 800*sqrt(2)
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Lines")
@@ -101,7 +105,7 @@ def start():
                 running = False
             else:
                 (xMouse, yMouse) = pygame.mouse.get_pos()
-                for i in range(n):
+                for i in range(0,40):
                     ll  = Line(xMouse, yMouse, xMouse + r*cos(delta*i), yMouse + r*sin(delta*i))
                     (x, y) = check_intersection(l, ll)
                     pygame.draw.line(window, white, (xMouse, yMouse), (x, y))
@@ -112,46 +116,91 @@ def start():
 # This works
 def tr():
     import pygame
-    import sys
     from pygame.time import delay
     from random import randint
     from numpy import pi, cos, sin, sqrt
+    from math import atan, tan
     black = (  0,  0,  0)
     white = (255,255,255)
-    n = 200
-    delta = pi/n*2
     pygame.init()
-    WIDTH  = 800
-    HEIGHT = 800
-    r      = 800*sqrt(2)
+    WIDTH  = 1920
+    HEIGHT = 1080
+    r      = max(WIDTH/2, HEIGHT)*sqrt(2)
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Lines")
     running = True
-    nObstacles = 5
-    obstacles = [Line(randint(0,WIDTH),randint(0,HEIGHT),randint(0,WIDTH),randint(0,HEIGHT)) for i in range(nObstacles)]
+    nObstacles  = 10
+    nRays       = 800
+    delta       = pi/nRays/4
+    obstacles = []
+    lX = 100
+    lY = 200
+    for i in range(nObstacles):
+        a = randint(0,WIDTH/2)
+        b = randint(0,HEIGHT)
+
+        obstacles.append(Line(a, b, a+randint(-lY, lY), b+randint(-lY, lY)))
+    obstacles.append(Line(WIDTH/2, 0, WIDTH/2, HEIGHT))
+    obstacles.append(Line(0, 0, 0, HEIGHT))
+    obstacles.append(Line(0, 0, WIDTH/2, 0))
+    obstacles.append(Line(0, HEIGHT, WIDTH/2, HEIGHT))
+    distances   = [max(WIDTH/2, HEIGHT) for i in range(nRays)]
+    boxes       = []
+    direction   = 0
+    xPlayer = WIDTH/4
+    yPlayer = HEIGHT/2
+    velocity= 1
+    deltaX  = WIDTH/2/nRays
+    maxTall= 200
+    movementX  = 0
     while running:
-        window = pygame.Surface((WIDTH, HEIGHT))
+        window = pygame.Surface((WIDTH/2, HEIGHT))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if pygame.key.get_pressed()[pygame.K_LEFT] or pygame.key.get_pressed()[pygame.K_a]:
+                movementX = -1
+            elif pygame.key.get_pressed()[pygame.K_UP] or pygame.key.get_pressed()[pygame.K_w]:
+                movementY = pi/2*3
+            elif pygame.key.get_pressed()[pygame.K_RIGHT] or pygame.key.get_pressed()[pygame.K_d]:
+                movementX = 1
+            elif pygame.key.get_pressed()[pygame.K_DOWN] or pygame.key.get_pressed()[pygame.K_s]:
+                movementY = pi/2
             else:
-                screen.fill(0)
-                (xMouse, yMouse) = pygame.mouse.get_pos()
-                # (xMouse, yMouse) = (200,330)
-                for i in range(n):
-                    x = xMouse + r*cos(delta*i)
-                    y = yMouse + r*sin(delta*i)
-                    ll  = Line(xMouse, yMouse, xMouse + r*cos(delta*i), yMouse + r*sin(delta*i))
-                    minDist = r
-                    indexDist = -1
-                    for i, l in enumerate(obstacles):
-                        (x1, y1) = check_intersection(l, ll)
-                        dist = check_dist(xMouse, x1, yMouse, y1)
-                        if dist < minDist:
-                            minDist = dist
-                            indexDist = i
-                            (x, y) = (x1, y1)
-                        pygame.draw.line(screen, white, (l.x1, l.y1), (l.x2, l.y2))
-                    pygame.draw.line(screen, white, (xMouse, yMouse), (x, y))
+                movementX = 0
+                movementY = 0
 
-        pygame.display.update()
+        screen.fill(0)
+        (xPlayer, yPlayer) = pygame.mouse.get_pos()
+
+        # xMP = xPointing - xPlayer
+        # yMP = yPointing - yPlayer
+
+        # (xMouse, yMouse) = (200,330)
+        # First screen
+        direction += movementX*0.1
+        for l in obstacles:
+            pygame.draw.line(screen, white, (l.x1, l.y1), (l.x2, l.y2))
+        for i in range(-int(nRays/2),int(nRays/2)):
+            x           = xPlayer + r*cos(delta*i + direction)
+            y           = yPlayer + r*sin(delta*i + direction)
+            ll          = Line(xPlayer, yPlayer, x, y)
+            minDist     = r
+            indexDist   = -1
+            for l in obstacles:
+                (x1, y1)    = check_intersection(l, ll)
+                dist        = check_dist(xPlayer, x1, yPlayer, y1)
+                if dist < minDist:
+                    minDist     = dist
+                    (x, y)      = (x1, y1)
+            pygame.draw.line(screen, white, (xPlayer, yPlayer), (x, y))
+            distances[i+int(nRays/2)] = minDist
+            # Second screen (3D)
+        for i in range(nRays):
+            x = deltaX*i + WIDTH/2
+            h = HEIGHT-distances[i]/sqrt(2)
+            surf    = pygame.Surface((1, h))
+            surf.fill((255,255,255))
+            surf.set_alpha(h*255/(HEIGHT*sqrt(2)))
+            screen.blit(surf, (x, (HEIGHT-h)/2))
+        pygame.display.flip()
